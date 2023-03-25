b762a47713ce474386ca0a34422935138b0d6216
sauravdani <sauravdani7@gmail.com>
2023-03-25 21:16:49 +0530

    MyToken
    It is my token

---
+++ MyToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract MyToken {
+
+    // public variables here
+    string public tokenName = "SOLONIUM";
+    string public tokenAbbrev = "SOL";
+    uint public totalSupply = 0;
+    
+    // mapping variable here
+    mapping(address => uint)public balances;
+
+    // mint function
+    function mint(address _address,uint _value)public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+    // burn function
+    function burn(address _address,uint _value)public {
+        if (balances[_address]>=_value){
+            totalSupply -= _value;
+            balances[_address] -= _value;
+        }
+    }
+}
